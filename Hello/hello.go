4dc42b5bfae099c34693029331919c607dbc8c79
Lucas Inocencio <piresilucas@gmail.com>
2022-05-03 21:22:23 -0300

    Finish first exercise in Go

--- Hello/hello.go
+++ Hello/hello.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -38,6 +43,7 @@ func exibeIntroducao() {
 
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("")
 }
 
 func leComando() int {
@@ -63,11 +69,13 @@ func exibeMenu() {
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 
-	sites := []string{
-		"https://random-status-code.herokuapp.com",
-		"https://www.alura.com.br",
-		"https://www.caelum.com.br",
-		"https://www.google.com.br"}
+	// sites := []string{
+	// 	"https://random-status-code.herokuapp.com",
+	// 	"https://www.alura.com.br",
+	// 	"https://www.caelum.com.br",
+	// 	"https://www.google.com.br"}
+
+	sites := lerSitesDoArquivo()
 
 	for i := 0; i < quantidadeMonitoramentos; i++ {
 		for _, site := range sites {
@@ -82,14 +90,69 @@ func iniciarMonitoramento() {
 }
 
 func testarSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu o erro ao fazer a requisição para o site:", err)
+	}
+
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
 func exibirLogs() {
 	fmt.Println("Exibindo logs...")
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao ler o arquivo de log:", err)
+	}
+
+	fmt.Println(string(arquivo))
+}
+
+func lerSitesDoArquivo() []string {
+	var sites []string
+	arquivo, err := os.Open("sitesSeremMonitorados.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao abrir o arquivo:", err)
+	}
+
+	leitorArquivo := bufio.NewReader(arquivo)
+
+	for {
+		// As aspas simples é pra informar que é o \n em byte
+		linha, err := leitorArquivo.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Ocorreu um erro ao fazer a leitura do arquivo:", err)
+		}
+	}
+
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, online bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(online) + "\n")
+
+	arquivo.Close()
 }
