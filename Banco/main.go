3f0e2a4dc99b0d30d79550ee6c449d8045338e7d
Lucas Inocêncio Pires <lucas.inocencio@timesharesolucoes.com.br>
2022-05-05 17:27:32 -0300

    Add withdraw method for bank struct

--- Banco/main.go
+++ Banco/main.go
@@ -9,32 +9,20 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
-func main() {
-	contaLucas := ContaCorrente{
-		titular:       "Lucas",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente!"
 	}
+}
 
-	// contaBruna := ContaCorrente{"Bruna", 345, 654321, 200.45}
+func main() {
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	fmt.Println(contaLucas)
-	// fmt.Println(contaBruna)
-
-	var contaCristina *ContaCorrente
-	contaCristina = new(ContaCorrente)
-	contaCristina.titular = "Cristina"
-	contaCristina.numeroAgencia = 349
-	contaCristina.numeroConta = 98765
-	contaCristina.saldo = 0.80
-
-	//var contaCristina2 *ContaCorrente
-	var contaCristina2 = new(ContaCorrente)
-	contaCristina2.titular = "Cristina"
-	contaCristina2.numeroAgencia = 349
-	contaCristina2.numeroConta = 98765
-	contaCristina2.saldo = 0.80
-
-	fmt.Println(contaCristina == contaCristina2)
+	fmt.Println(contaDaSilvia.Sacar(800))
 }
